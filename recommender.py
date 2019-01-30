4343ea8081b9d28f11976cbfcf16ecbff7e17e7e
Krishna Chaitanya Pullakandam <chaitu.2037@gmail.com>
2019-01-30 12:10:42 -0500

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -1,4 +1,19 @@
-# Import libraries
+# # Music Recommender System using Apache Spark and Python
+# 
+# ## Description
+# 
+# For this project, I created a recommender system that would recommend new musical artists to a user based on their listening history. Suggesting different songs or musical artists to a user is important to many music streaming services, such as Pandora and Spotify. In addition, this type of recommender system could also be used as a means of suggesting TV shows or movies to a user (e.g., Netflix). 
+# 
+# To create this system I used Spark and the collaborative filtering technique. 
+# **Submission Instructions:** 
+ 
+# ## Datasets
+# 
+# I have used publicly available song data from audioscrobbler, which can be found [here](http://www-etud.iro.umontreal.ca/~bergstrj/audioscrobbler_data.html). However, I modified the original data files so that the code would run in a reasonable time on a single machine.
+
+
+# ## Necessary Package Imports
+
 import findspark
 findspark.init()
 
@@ -14,7 +29,9 @@ sc.stop()
 sc = SparkContext('local','Recommender')
 
 
-# Import textfile from location into RDD variables 
+# ## Loading data
+# Loading the three datasets into RDDs and name them `artistData`, `artistAlias`, and `userArtistData`. 
+
 artistData = sc.textFile("data_raw/artist_data_small.txt")
 artistAlias = sc.textFile("data_raw/artist_alias_small.txt")
 userArtistData = sc.textFile("data_raw/user_artist_data_small.txt")
@@ -54,8 +71,11 @@ temp_list = sorted(my_list, key=itemgetter(1))
 for list_elems in reversed(temp_list):
     print("User "+str(int(list_elems[0]))+" has a total play count of "+str(int(list_elems[1]))+" and a mean play count of "+str(int(list_elems[2])))
     
+
+
+# ####  Splitting Data for Testing
     
-    # Split the 'userArtistData' dataset into training, validation and test datasets. Store in cache for frequent access
+# Split the 'userArtistData' dataset into training, validation and test datasets. Store in cache for frequent access
 trainData,validationData,testData=userArtistDataNew.randomSplit([0.4,0.4,0.2],13)
 
 trainData.cache()
@@ -70,6 +90,13 @@ print(trainData.count())
 print(validationData.count())
 print(testData.count())
 
+# ## The Recommender Model
+ 
+# ### Model Evaluation
+# 
+# Although there may be several ways to evaluate a model, I have used a simple method here. Suppose we have a model and some dataset of *true* artist plays for a set of users. This model can be used to predict the top X artist recommendations for a user and these recommendations can be compared the artists that the user actually listened to (here, X will be the number of artists in the dataset of *true* artist plays). Then, the fraction of overlap between the top X predictions of the model and the X artists that the user actually listened to can be calculated. This process can be repeated for all users and an average value returned.
+
+# The function `modelEval` will take a model (the output of ALS.trainImplicit) and a dataset as input. Parameter Tuning is done one the validation data.After parameter tuning, the model can be evaluated on the test data.
 
 
 def modelEval(model, dataset):
@@ -115,10 +142,7 @@ def modelEval(model, dataset):
     print(upon/float(len(users_list)))    
 
     
-    
-    # Print average score of the model for all users for the specified rank
-    # YOUR CODE GOES HERE
-    
+# ### Model Construction
     
 rankList = [2,10,20]
 for rank in rankList:
@@ -131,7 +155,10 @@ bestModel = ALS.trainImplicit(trainData, rank=10, seed=345)
 modelEval(bestModel, testData) 
 
 
-# Find the top 5 artists for a particular user and list their names
+
+# ## Trying Some Artist Recommendations
+# Using the best model above, predicting the top 5 artists for user `1059637` using the [recommendProducts](http://spark.apache.org/docs/1.5.2/api/python/pyspark.mllib.html#pyspark.mllib.recommendation.MatrixFactorizationModel.recommendProducts) function.
+
 x = bestModel.recommendProducts(1059637,5)
 recomendations = sc.parallelize(x)
 recomendationArtists = recomendations.map(lambda r:r[1]).collect()
